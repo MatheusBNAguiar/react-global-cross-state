55bc5da5fe4a850a254743b0132fce75e64915df
Matheus Barreto <matheus.bnaguiar@gmail.com>
2022-08-27 13:15:49 -0300

    chore: Add better bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
-
+import path from 'path'
 const entrypoint = 'src/index.ts'
+
+const { root } = path.parse(process.cwd())
+
+function external(id) {
+  return !id.startsWith('.') && !id.startsWith(root)
+}
 
 export default [
   {
     input: entrypoint,
+    external,
     plugins: [esbuild()],
     output: [
       {
-        file: 'dist/index.cjs.js',
+        file: 'dist/index.js',
         format: 'cjs',
         sourcemap: true,
       },
@@ -17,6 +24,7 @@ export default [
   },
   {
     input: entrypoint,
+    external,
     plugins: [dts()],
     output: [
       {
