1102e36dbb23941eacbb6f1c20077520cedc2093
Matheus Barreto <matheus.bnaguiar@gmail.com>
2022-08-27 11:05:41 -0300

    chore: Start adding types

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+
+const entrypoint = `src/index.ts`
+
+export default [
+  {
+    input: entrypoint,
+    plugins: [esbuild()],
+    output: [
+      {
+        file: `dist/index.cjs.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ]
+  },
+  {
+    input: entrypoint,
+    plugins: [dts()],
+    output: [
+      {
+        file: `dist/index.d.ts`,
+        format: 'es',
+      },]
+  }
+]
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function TestSetup(text: string){
+console.log(text)
+}
